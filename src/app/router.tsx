e99b6298c343b39c121f680e756a30e0a770ddd4
zzuhann <zzuhanlin@gmail.com>
2024-03-03 22:38:02 +0800

    add app folder and router

---
+++ src/app/router.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <div>Home</div>,
+	},
+]);
+
+export default router;
